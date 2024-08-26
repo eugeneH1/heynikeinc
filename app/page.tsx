cac3e12a9c429ad01559092c07c7ff57167c419d
Eugene Heynike <eugeneheynike@Eugenes-MBP.lan>
2024-08-26 16:28:01 +0200

    basics of the hero done. nav to hero and blog done

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,11 @@ import BlogPage from "./components/Blog";
 export default function Home() {
   return (
     <>
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main id='home'className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
+          Get started by emailing&nbsp;
+          <a href="mailto:law@heynikeinc.co.za"><strong>law@heynikeinc.co.za</strong></a>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <a
@@ -22,11 +22,11 @@ export default function Home() {
           >
             By{" "}
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
+              src="/LogoBlack.svg"
+              alt="Nexablend Digital Logo"
               className="dark:invert"
-              width={100}
-              height={24}
+              width={240}
+              height={240}
               priority
             />
           </a>
@@ -36,10 +36,10 @@ export default function Home() {
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
+          src="/heynikeinc.png"
           alt="Next.js Logo"
-          width={180}
-          height={37}
+          width={400}
+          height={400}
           priority
         />
       </div>
@@ -52,13 +52,13 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
+            Commercial{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
+            Expert legal services in commercial law, including contracts, transactions, and dispute resolution.
           </p>
         </a>
 
@@ -69,13 +69,13 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
+            Contract{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+            Specialized in drafting, reviewing, and enforcing contracts.
           </p>
         </a>
 
@@ -86,13 +86,13 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
+            Property{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
+            Assistance with property transactions, leases, and conveyancing.
           </p>
         </a>
 
@@ -103,19 +103,21 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
+            3%.com{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            We are 3%.com Properties
           </p>
         </a>
       </div>
     </main>
     <Separator />
-    <BlogPage />
+    <div id="blog">    
+      <BlogPage />
+    </div>
     </>
   );
 }
