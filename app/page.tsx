19fbb9d0b306a75580b1720d268651d2ad374ab8
Eugene Heynike <eugeneheynike@Eugenes-MacBook-Pro.local>
2024-08-28 11:51:11 +0200

    cards are done for now maybe add some images

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import { Hero } from "@/components/Hero";
 
 export default function Home() {
   return (
+    <>
     <main id='home'className="flex min-h-screen flex-col items-center justify-between p-24">
       {/* <Separator /> */}
       <Hero />
@@ -18,10 +19,11 @@ export default function Home() {
       </div>
 
       <Separator />
-      <div id="blog">    
-        <BlogPage />
-      </div> 
-      </main>
-    
-  );
+       
+    </main>
+    <div id="blog">    
+      <BlogPage />
+    </div> 
+      </>
+    );
 }
--- components/TabsSection.tsx
+++ components/TabsSection.tsx
@@ -6,12 +6,12 @@ import { Tabs } from "./ui/tabs";
 export function TabsSection() {
   const tabs = [
     {
-      title: "Attorney Agents",
-      value: "attorney-agents",
+      title: "Commercial & Business Department",
+      value: "commercial-business",
       content: (
-        <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
-          <p>Attorney Agents</p>
-          <DummyContent />
+        <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-[#002f6c] to-[#008080]">
+          <p>Commercial & Business Department</p>
+            <BusinessContent />
         </div>
       ),
     },
@@ -19,19 +19,19 @@ export function TabsSection() {
       title: "Conveyancing Department",
       value: "conveyancing",
       content: (
-        <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
+        <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-[#008080] to-[#98ff98]">
           <p>Conveyancing Department</p>
-          <DummyContent />
+            <ConveyancingContent />
         </div>
       ),
     },
     {
-      title: "Commercial & Business",
-      value: "commercial-business",
+      title: "Attorney Agents",
+      value: "attorney-agents",
       content: (
-        <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
-          <p>Commercial & Business</p>
-          <DummyContent />
+        <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-[#98ff98] to-[#fffacd]">
+          <p>Attorney Agents</p>
+            <AttorneyContent />
         </div>
       ),
     },
@@ -39,9 +39,9 @@ export function TabsSection() {
       title: "Collections Department",
       value: "collections",
       content: (
-        <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
+        <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-[#fffacd] to-[#ff7f50]">
           <p>Collections Department</p>
-          <DummyContent />
+            <CollectionsContent />
         </div>
       ),
     },
@@ -54,10 +54,41 @@ export function TabsSection() {
   );
 }
 
-const DummyContent = () => {
+const AttorneyContent = () => {
   return (
     <div className="w-full h-full  text-xl">
-        <p>Dummy Content</p>
+        <br/>
+        <p>And of course, the team of Property Consultants managing the sales for the firm.</p>
     </div>
   );
 };
+
+const BusinessContent = () => {
+    return (
+      <div className="w-full h-full  text-xl">
+          <br/>
+          <p>Firstly there is the litigation department consisting of Maartens Heynike a senior attorney with nearly 40 years of experience , 
+            and his son and candidate attorney Daniel Heynike, who assists him in both law and business. 
+            Also aided by Merle Capotis a highly competent PA.</p>
+      </div>
+    );
+  };
+  
+  const ConveyancingContent = () => {
+    return (
+      <div className="w-full h-full  text-xl">
+          <br/>
+          <p>Secondly the conveyancing team, comprised of Bronwen Gordon and her conveyancing secretary Pauline.</p>
+      </div>
+    );
+  }
+
+  const CollectionsContent = () => {
+    return (
+      <div className="w-full h-full  text-xl">
+        <br/>
+          <p>Finally, Heynike Inc also has a small debt collections department, Mary and Sandra have been working for Heynike Inc 
+            and running this department for over 20 years, with immense experience their success trumps firms with double the staff.</p>
+      </div>
+    );
+  }
